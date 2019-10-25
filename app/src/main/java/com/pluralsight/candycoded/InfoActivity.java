65aed93fb1a675c403585b0b9fe22010a0c1dd3e
Sherman Mathopa <shermanmolele@gmail.com>
2019-10-25 16:40:06 +0200

    Created phone intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,4 +45,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Uri telephoneUri = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(telephoneUri);
+        startActivity(dialIntent);
+    }
 }
